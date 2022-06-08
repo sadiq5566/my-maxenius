f2a1a80857ac0b6ff0b4b6c603e97b43b63e3843
sadiq <buttsadiq15@gmail.com>
2022-06-08 11:42:31 +0500

    commit for chk husky 3

--- components/ChkHusky.js
+++ components/ChkHusky.js
@@ -3,6 +3,9 @@ import React from 'react'
 const ChkHusky = () => {
     return (
         <div>
+            sdad
+        </div>
+        <div>
             check Huskyasdasad sa
         </div>
     )
