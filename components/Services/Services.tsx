50e68c5b5c620e47310f90bcf2c6788e99505847
sadiq <buttsadiq15@gmail.com>
2022-06-23 19:12:01 +0500

    service page

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -1,16 +1,57 @@
 import React from "react";
 import { servicesData } from "../../data/Services";
 import StraightArrowWhiteSvg from "../../public/assets/svgs/StraightArrowWhiteSvg";
+import { useRouter } from "next/router";
 export const Services = () => {
+  const router = useRouter();
+
   return (
-    <div className="w-full h-[1423px] md:h-[2000px] sm:h-auto pt-20 bg-serviceBg ">
+    <div className="w-full h-[1300px] md:h-[2000px] sm:h-auto pt-20 bg-serviceBg ">
       <section className="body-font">
-        <div className="container px-5 pt-24 pb-16 mx-auto">
-          <div className="flex flex-col text-center sm:text-left sm:px-8 w-full mb-20">
-            <h1 className="font-poppins font-normal text-4xl not-italic sm:text-8xl leading-[48px] text-black">
-              Services We Offer
-            </h1>
-          </div>
+        <div
+          className={`container px-5 ${
+            router.pathname === "/services" ? "pt-0" : "pt-24"
+          }  pb-16 mx-auto`}
+        >
+          {router.pathname === "/services" ? (
+            <div className="flex flex-col text-center sm:text-left sm:px-8 w-full mb-20">
+              <div className="mb-4">
+                <h3 className="font-Lato font-medium text-xl leading-9 text-pColor sm:text-center">
+                  What we do?
+                </h3>
+              </div>
+              <div>
+                <h1 className="text-black font-poppins font-normal text-4xl sm:text-center">
+                  Solve Your Toughest Challenges
+                </h1>
+              </div>
+              <section className="">
+                <div className=" px-5 pt-10 mx-auto flex flex-wrap flex-col">
+                  <div className="flex flex-wrap mx-48">
+                    <a className=" py-1 w-1/4 justify-center text-black text-base border-b-2 font-semibold bg-white inline-flex items-center border-main rounded-full sm:w-full sm:px-auto sm:border-b-0 sm:mb-4 sm:text-xl">
+                      Software Development
+                    </a>
+                    <a className=" py-1 w-1/4 text-pColor text-base font-normal justify-center  border-b-2 inline-flex items-center sm:w-full sm:px-auto sm:mb-4 sm:text-xl">
+                      Consultancy
+                    </a>
+                    <a className=" py-1 w-1/4 justify-center  border-b-2 text-pColor text-base font-normal inline-flex items-center sm:w-full sm:px-auto sm:mb-4 sm:text-xl">
+                      Enterprise Solutions
+                    </a>
+                    <a className=" py-1 w-1/4 justify-center border-b-2 inline-flex items-center text-pColor text-base font-normal sm:w-full sm:px-auto sm:mb-4 sm:text-xl">
+                      Emerging Technologies
+                    </a>
+                  </div>
+                </div>
+              </section>
+            </div>
+          ) : (
+            <div className="flex flex-col text-center sm:text-left sm:px-8 w-full mb-20">
+              <h1 className="font-poppins font-normal text-4xl not-italic sm:text-8xl leading-[48px] text-black">
+                Services We Offer
+              </h1>
+            </div>
+          )}
+
           <div className="flex flex-wrap mx-4 md:mx-24">
             {servicesData.map((service) => {
               return (
@@ -42,16 +83,20 @@ export const Services = () => {
             })}
           </div>
         </div>
-        <div className="w-full flex justify-center items-center sm:my-12">
-          <div className="flex h-[54px] w-[230px] sm:h-[100px] sm:w-[350px] sm:rounded-full mb-3  bg-gradient-to-r from-gradient1 to-gradient2  text-white rounded-4xl justify-center items-center cursor-pointer">
-            <span className="text-white text-base leading-4 font-bold sm:text-2xl sm:font-bold font-poppins">
-              View All Services
-            </span>
-            <div className="h-[24px] w-[24px] sm:h-[50px] sm:w-[50px]  ml-2 flex justify-center items-center">
-              <StraightArrowWhiteSvg />
+        {router.pathname === "/services" ? (
+          ""
+        ) : (
+          <div className="w-full flex justify-center items-center sm:my-12">
+            <div className="flex h-[54px] w-[230px] sm:h-[100px] sm:w-[350px] sm:rounded-full mb-3  bg-gradient-to-r from-gradient1 to-gradient2  text-white rounded-4xl justify-center items-center cursor-pointer">
+              <span className="text-white text-base leading-4 font-bold sm:text-2xl sm:font-bold font-poppins">
+                View All Services
+              </span>
+              <div className="h-[24px] w-[24px] sm:h-[50px] sm:w-[50px]  ml-2 flex justify-center items-center">
+                <StraightArrowWhiteSvg />
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </section>
     </div>
   );
