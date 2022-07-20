1c9095a967b482efbd45e69b4a84c4e8c8a09060
Muqaddaskhalil08 <93579258+Muqaddaskhalil08@users.noreply.github.com>
2022-07-20 18:18:37 +0500

    fix height

--- pages/ourwork.tsx
+++ pages/ourwork.tsx
@@ -9,6 +9,7 @@ const OurWork = () => {
         headTitle="Take A Look At Our Work"
         subTitle="Here are some of our projects that we take pride in. Take a look around and you'll definitely find something fascinating in them"
         bannerImageClass="hidden sm:block"
+        customHeight="h-[654px] sm:h-[510.65px]"
       >
         <Blog />
       </Layout>
